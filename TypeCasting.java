b16975e10635e50794cbfef423cf65f2d9fc4a2e
Valval-ui <valerieamba06@gmail.com>
2025-03-20 16:07:31 +0100

    UserInput

--- TypeCasting.java
+++ TypeCasting.java
@@ -9,5 +9,10 @@ public class TypeCasting{
 		
 		int asciiCode = (char)alpha;
 		System.out.printf("The ASCII code for ? is %d%n",asciiCode);
+		
+		double height = 5.8;
+		
+		int myHeight = (int)height;
+		System.out.printf("My faculty's height is %d%n",myHeight);
 	}
 }
